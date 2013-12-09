b1ae61219ee9df1feaa467d67f3a1e35715caeec
Gregory S. Kielian <gregory.kielian@gmail.com>
2013-12-09 15:35:50 -0800

    Update RaspberryPi_setup.sh

--- RaspberryPi_setup.sh
+++ RaspberryPi_setup.sh
@@ -8,5 +8,19 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #Get Espeak and prepare Speaker Outputs
+sudo apt-get install -y alsa-utils
 sudo apt-get install -y espeak
-amixer
+amixer cset numid=3 1
+
+#set up twitter
+sudo apt-get install -y python-setuptools 
+sudo easy_install twitter
+
+## NOTE: You will still need to authenticate the account 
+# by typing later:
+#twitter -e youremailaddress@email.com set "Hello World"
+
+
+
+
+
