3471a028dff58f21e1af80a9f5cdd3b8e8eeb544
root <root@kielian.net>
2014-02-28 03:43:07 -0500

    created an example api request w/o api :key:

--- google-maps-api/public_request.sh
+++ google-maps-api/public_request.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-curl "https://maps.googleapis.com/maps/api/directions/json?origin=Chicago,IL&destination=Los+Angeles,CA&waypoints=Joplin,MO|Oklahoma+City,OK&sensor=false&key=YOUR_API_KEY"
+# more instructions here: https://developers.google.com/maps/documentation/directions/
+
+#starting coordinates
+lat_s=37.794228
+long_s=-122.404477
+
+#destination coordinates
+lat_d=37.762685
+long_d=-122.419253
+
+
+#api request
+curl "https://maps.googleapis.com/maps/api/directions/json?origin=$lat_s,$long_s&destination=$lat_d,$long_d&mode=walking&sensor=false&key=YOUR_API_KEY"
 
 
 # basically we curl for directions, from this server's static ip
