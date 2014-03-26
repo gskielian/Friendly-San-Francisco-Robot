8b95953da6f4414be1ea4bb33167327140916780
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-03-26 00:44:19 -0700

    Planned Skeleton

---
+++ Python-Code/LatestGPS.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#TODO test out the typical way of doing this
+
+#TODO use python command line manipulation to query server for directions
+
+#TODO use python command line manipulation and jq to parse the reply
+
+#TODO use python command line manipulation to ask for directions.
